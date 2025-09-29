b08a45cab060f48d9eba27af6378513bc4bc648d
Tude Prayatna <tudeprayatna04@gmail.com>
2025-09-29 22:40:15 +0800

    feat: adjust theme and colors

--- app/src/main/java/com/prayatna/lookiesapp/ui/theme/Color.kt
+++ app/src/main/java/com/prayatna/lookiesapp/ui/theme/Color.kt
@@ -2,10 +2,44 @@ package com.prayatna.lookiesapp.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val PrimaryBase = Color(0xFF18186C) // #18186C
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+// Light Theme
+val light_primary = PrimaryBase
+val light_onPrimary = Color(0xFFFFFFFF)
+val light_primaryContainer = Color(0xFF4C4CB0)
+val light_onPrimaryContainer = Color(0xFFFFFFFF)
+
+val light_secondary = Color(0xFF4E5BA6)
+val light_onSecondary = Color(0xFFFFFFFF)
+val light_secondaryContainer = Color(0xFFD0D3F0)
+val light_onSecondaryContainer = Color(0xFF1A1C40)
+
+val light_background = Color(0xFFFFFFFF)
+val light_onBackground = Color(0xFF1A1B1E)
+
+val light_surface = Color(0xFFFFFFFF)
+val light_onSurface = Color(0xFF1A1B1E)
+
+val light_error = Color(0xFFBA1A1A)
+val light_onError = Color(0xFFFFFFFF)
+
+// Dark Theme
+val dark_primary = Color(0xFF8080FF)
+val dark_onPrimary = Color(0xFF18186C)
+val dark_primaryContainer = Color(0xFF2D2D80)
+val dark_onPrimaryContainer = Color(0xFFCCCCFF)
+
+val dark_secondary = Color(0xFF9CA3DB)
+val dark_onSecondary = Color(0xFF18186C)
+val dark_secondaryContainer = Color(0xFF303370)
+val dark_onSecondaryContainer = Color(0xFFDBDEFF)
+
+val dark_background = Color(0xFF121212)
+val dark_onBackground = Color(0xFFE6E6E6)
+
+val dark_surface = Color(0xFF1E1E1E)
+val dark_onSurface = Color(0xFFE6E6E6)
+
+val dark_error = Color(0xFFFFB4AB)
+val dark_onError = Color(0xFF690005)
